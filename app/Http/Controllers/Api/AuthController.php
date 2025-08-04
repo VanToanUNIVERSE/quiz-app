9301ca006b9c079de9eac0974ad8b09015537f4d
Toan <146208081+VanToanUNIVERSE@users.noreply.github.com>
2025-08-04 16:45:41 +0700

    update register has image

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,22 +5,31 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
 
         $validated = $request->validate([
             'username' => 'required|unique:users|string|max:255|min: 5',
             'password' => 'required|string|min:5',
-            'fullName' => 'required|string|min:5'
+            'fullName' => 'required|string|min:5',
+            'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
         ]);
+
+        $imagePath = null;
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('uploads', 'public'); // lưu vào storage/app/public/uploads
+        }
 
         $data = [
             'username' => $validated['username'],
             'password' => Hash::make($validated['password']),
-            'fullName' => $validated['fullName']
+            'fullName' => $validated['fullName'],
+            'image' => $imagePath
         ];
 
         $user = User::create($data);
@@ -30,5 +39,26 @@ class AuthController extends Controller
             'user' => $user,
             'status' => 'success'
         ]);
+    }
+
+    public function login(Request $request)
+    {
+        $info = $request->only('username', 'password');
+        if (!Auth::attempt($info)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+        $user = Auth::user();
+        return response()->json([
+            'token' => $user->createToken('token')->plainTextToken,
+            'message' => 'Login success',
+            'user' => $user,
+            'status' => 'success'
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out']);
     }
 }
