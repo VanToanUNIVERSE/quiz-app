25ad7a76e63f7832cb60c8d108a077ebd5ca11cf
Toan <146208081+VanToanUNIVERSE@users.noreply.github.com>
2025-08-04 11:54:55 +0700

    coplete login demo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import InputGroup from '../components/InputGroup';
 import { validate } from '../helpers/validators';
+import Message from '../components/Message';
 
 const Login = () => {
     const [error, setError] = useState({});
+    const [response, setResponse] = useState({
+        message: '',
+        status: '',
+    });
     const [form, setForm] = useState({ password: '' });
     const handleValidate = (e) => {
         validate(e, form, setForm, setError);
@@ -25,17 +30,27 @@ const Login = () => {
         fetch('http://127.0.0.1:8000/api/login', {
             method: 'POST',
             headers: {
+                'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                'username' : document.getElementById('username').value,
-                'password' : document.getElementById('password').value
+                'username': document.getElementById('username').value,
+                'password': document.getElementById('password').value
             })
         }).then(res => res.json())
-        .then(data => console.log(data.status))
-        .catch(err => console.log("Error: ", err));
-        
+            .then(data => {
+                setResponse({ message: data.message, status: data.errors });
+                if (!data.errors) {
+                    // Lưu user vào localStorage
+                    localStorage.setItem('user', JSON.stringify(data.user));
+                }
+            })
+            .catch(err => console.log("Error: ", err));
+
     };
+    const invisible = () => {
+        setResponse({ message: '', status: '' });
+    }
     return (
         <div className='p-1'>
             <form className='flex flex-col gap-1 p-10 w-[40%] shadow mx-auto mt-20 justify-center items-center rounded' method='post' onSubmit={handleSubmit}>
@@ -46,7 +61,8 @@ const Login = () => {
                     <button id='submit-btn' type='submit' className='px-7 py-1 bg-blue-500 hover:bg-blue-400 rounded cursor-pointer'>Login</button>
                     <Link to="/register" className=' underline' >Register</Link>
                 </div>
-                <p>{error.typical}</p>
+                <Message onClick={invisible} visible={response.message != '' ? '' : 'invisible'} message={response.message || 'The account has been created'} to={response.status ? '/register' : '/'}></Message>
+
             </form>
         </div>
     );
