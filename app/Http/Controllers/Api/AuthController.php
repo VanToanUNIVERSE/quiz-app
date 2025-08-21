74ec8d1fac74dda4e11ac997d3cb4e8411055f23
Toan <146208081+VanToanUNIVERSE@users.noreply.github.com>
2025-08-21 14:26:32 +0700

    Update image validation in register method to require an image

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,12 +12,13 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-
         $validated = $request->validate([
             'username' => 'required|unique:users|string|max:255|min: 5',
             'password' => 'required|string|min:5',
             'fullName' => 'required|string|min:5',
-            'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
+            'image' => 'required|image|mimes:jpg,jpeg,png,webp|max:2048'
+        ], [
+            'image.required' => 'Please select a picture'
         ]);
 
         $imagePath = null;
