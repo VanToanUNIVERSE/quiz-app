857b78a60597aae09974050672718c5850eccb4e
Toan <146208081+VanToanUNIVERSE@users.noreply.github.com>
2026-08-21 20:06:06 +0700

    fix: hide password from user model serialization

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -11,6 +11,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable; 
     protected $fillable = ['username', 'password', 'fullName', 'image'];
+    protected $hidden = ['password'];
     public function collections() {
         return $this->hasMany(Collection::class);
     }
